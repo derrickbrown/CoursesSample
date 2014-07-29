6df353fcce0c1b591fe6955af3158b4814bdb677
derrickbrown <JobAdder@JobAdders-iMac-4.local>
2014-07-29 10:33:08 -0600

    Added UI

--- CoursesiOS/CourseViewController.designer.cs
+++ CoursesiOS/CourseViewController.designer.cs
@@ -1,18 +1,42 @@
 ﻿// WARNING
 //
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using MonoTouch.Foundation;
+using System.CodeDom.Compiler;
 
 namespace CoursesiOS
 {
 	[Register ("CourseViewController")]
 	partial class CourseViewController
 	{
+		[Outlet]
+		MonoTouch.UIKit.UIButton buttonNext { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIButton buttonPrev { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UILabel labelTitle { get; set; }
+		
 		void ReleaseDesignerOutlets ()
 		{
+			if (buttonPrev != null) {
+				buttonPrev.Dispose ();
+				buttonPrev = null;
+			}
+
+			if (buttonNext != null) {
+				buttonNext.Dispose ();
+				buttonNext = null;
+			}
+
+			if (labelTitle != null) {
+				labelTitle.Dispose ();
+				labelTitle = null;
+			}
 		}
 	}
 }
