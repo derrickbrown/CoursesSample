dfd9d8b4d191fba5332748dfb4d0b069126f31c5
derrickbrown <JobAdder@JobAdders-iMac-4.local>
2014-07-29 10:42:19 -0600

    updated viewController

---
+++ CoursesiOS/CoursesViewController.cs
@@ -0,0 +1,14 @@
+using System;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using System.CodeDom.Compiler;
+
+namespace CoursesiOS
+{
+	partial class CoursesViewController : UIViewController
+	{
+		public CoursesViewController (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
